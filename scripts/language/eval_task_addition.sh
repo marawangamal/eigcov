532be8200f39fa2113a7ab6e169753656a87d143
Marawan Gamal <marawan.gamal@live.com>
2026-04-06 18:27:27 -0400

    Update results

--- scripts/language/eval_task_addition.sh
+++ scripts/language/eval_task_addition.sh
@@ -17,8 +17,8 @@ export PYTHONPATH="$PYTHONPATH:$PWD"
 export SSL_CERT_DIR=/etc/ssl/certs
 
 # ===== Default experiments (no hyperparameter tuning) =====
-MODELS=(t5-base t5-large)
-METHODS=(isoc)
+MODELS=(t5-large)
+METHODS=(eigcov tsv)
 FT_MODE=standard
 HPO=""
 
